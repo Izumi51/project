d59bd7ad09075d951d3a181881a2224a181954d0
Izumi51 <joaoizumi27@gmail>
2025-10-13 21:51:29 -0300

    init: Adding first build of RESTAPI

---
+++ api/src/test/java/com/projectNI/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectNI.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
